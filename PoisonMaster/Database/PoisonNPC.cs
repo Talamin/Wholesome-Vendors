8469c4d5be7ad6011bed9b2caee0935b7e51b442
Daniel Marx <dancingdog81@hotmail.com>
2021-02-17 14:37:18 +0100

    Added all  PoisonVendors

--- PoisonMaster/Database/PoisonNPC.cs
+++ PoisonMaster/Database/PoisonNPC.cs
@@ -11,14 +11,15 @@ public class PoisonNPC
     public int id { get; set; }
     public Vector3 Position { get; set; }
     public string Name { get; set; }
-
+    public string SubName { get; set; }
     public ContinentId Continent { get; set;}
 
-    public PoisonNPC(int ID, Vector3 POSITION, string NAME, ContinentId CONTINENT)
+    public PoisonNPC(int ID, Vector3 POSITION,ContinentId CONTINENT, string NAME, string SUBNAME)
     {
         id = ID;
         Position = POSITION;
         Name = NAME;
         Continent = CONTINENT;
+        SubName = SUBNAME;
     }
 }
