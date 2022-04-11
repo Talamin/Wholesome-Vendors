ed58b5da56c26ceb6db8a56f92f9668c457bc331
Nico <jeanlouisledaron@gmail.com>
2022-04-11 18:27:26 +0200

    Shared module

--- Wholesome_Vendors/Main.cs
+++ Wholesome_Vendors/Main.cs
@@ -25,7 +25,7 @@ public class Main : IPlugin
 
     private Timer stateAddTimer;
 
-    public static string version = "1.3.03"; // Must match version in Version.txt
+    public static string version = "1.3.04"; // Must match version in Version.txt
 
     public void Initialize()
     {
